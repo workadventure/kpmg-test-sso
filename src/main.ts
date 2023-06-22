05cb1c0c169ab9e24e16cc15a3983e9497356115
Nolway <faizeau.alexis@gmail.com>
2023-06-22 14:40:57 +0200

    Init

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,183 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import axios, { AxiosInstance } from "axios";
+import { z } from "zod";
+
+const SSOToken = z.object({
+    oid: z.string(),
+});
+
+type SSOToken = z.infer<typeof SSOToken>;
+
+const TeamsAvailability = z.enum([
+    "Available",
+    "AvailableIdle",
+    "Away",
+    "BeRightBack",
+    "Busy",
+    "BusyIdle",
+    "DoNotDisturb",
+    "Offline",
+    "PresenceUnknown"
+]);
+
+type TeamsAvailability = z.infer<typeof TeamsAvailability>;
+
+const TeamsActivity = z.enum([
+    "Available",
+    "Away",
+    "BeRightBack",
+    "Busy",
+    "DoNotDisturb",
+    "InACall",
+    "InAConferenceCall",
+    "Inactive",
+    "InAMeeting",
+    "Offline",
+    "OffWork",
+    "OutOfOffice",
+    "PresenceUnknown",
+    "Presenting",
+    "UrgentInterruptionsOnly"
+]);
+
+type TeamsActivity = z.infer<typeof TeamsActivity>;
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
+let teamsStatusCheckInterval: NodeJS.Timer|undefined = undefined;
 
-// Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    const metadataIsOk = z.object({
+        player: z.object({
+            accessTokens: z.array(z.object({
+                token: z.string()
+            }))
+        })
+    }).safeParse(WA.metadata);
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    if (metadataIsOk.success && metadataIsOk.data.player.accessTokens.length > 0) {
+        const userToken = metadataIsOk.data.player.accessTokens[0].token;
+        let tokenData: SSOToken;
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+        try {
+            tokenData = parseJwt(userToken);
+        } catch (err) {
+            throw new Error("Your token is not valid");
+        }
 
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+        const msClient = axios.create({
+            baseURL: 'https://graph.microsoft.com/v1.0',
+            headers: {
+                "Authorization": `Bearer ${userToken}`,
+                "Content-Type": "application/json"
+            }
+        });
 
+        startTeamsStatusCheckInterval(msClient, tokenData);
+
+        WA.player.proximityMeeting.onJoin().subscribe(() => {
+            clearInterval(teamsStatusCheckInterval);
+            setTeamsStatus(msClient, tokenData, "Busy", "Busy");
+        });
+
+        WA.player.proximityMeeting.onLeave().subscribe(() => {
+            setTeamsStatus(msClient, tokenData, "Available", "Available");
+            startTeamsStatusCheckInterval(msClient, tokenData);
+        });
+    } else {
+        console.info("Your are not connected with the KPMG SSO");
+    }
 }).catch(e => console.error(e));
 
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+/**
+ * Parse the JWT token
+ * @param token
+ * @returns
+ */
+function parseJwt(token: string): SSOToken {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return SSOToken.parse(JSON.parse(jsonPayload));
 }
 
-export {};
+function startTeamsStatusCheckInterval(client: AxiosInstance, token: SSOToken) {
+    teamsStatusCheckInterval = setInterval(() => {
+        checkTeamsStatus(client, token);
+    }, 60000);
+}
+
+/**
+ * Check the Teams status of the user
+ * @param client
+ * @param token
+ */
+function checkTeamsStatus(client: AxiosInstance, token: SSOToken) {
+    client.get('/me/presence').then(response => {
+        const userPresence = response.data;
+        const isPresenceData = z.object({
+            availability: TeamsAvailability,
+            activity: TeamsActivity,
+        }).safeParse(userPresence);
+
+        if (!isPresenceData.success) {
+            throw new Error("Your presence status cannot be handled by this script");
+        }
+
+        const notAvailable: TeamsAvailability[] = ["BeRightBack", "Busy", "DoNotDisturb"];
+
+        if (notAvailable.includes(isPresenceData.data.availability)) {
+            WA.controls.disablePlayerProximityMeeting();
+            return;
+        }
+
+        const available = ["Available", "Away", "Offline"];
+
+        if (available.includes(isPresenceData.data.availability)) {
+            WA.controls.restorePlayerProximityMeeting();
+
+            if (isPresenceData.data.availability === "Offline") {
+                setTeamsStatus(client, token, "Available", "Available", "PT2M");
+            }
+        }
+    }).catch(e => console.error("Error while getting Teams status", e));
+}
+
+/**
+ * Set the Teams status of the user
+ * @param client
+ * @param token
+ * @param availability
+ * @param activity
+ * @param duration
+ */
+function setTeamsStatus(
+        client: AxiosInstance,
+        token: SSOToken,
+        availability: Exclude<TeamsAvailability, "AvailableIdle" | "BusyIdle" | "PresenceUnknown">,
+        activity: Exclude<TeamsActivity,
+            "InACall" |
+            "InAConferenceCall" |
+            "Inactive" |
+            "InAMeeting" |
+            "OffWork" |
+            "OutOfOffice" |
+            "Presenting" |
+            "UrgentInterruptionsOnly"
+        >,
+        duration?: string
+) {
+    client.post(`/users/${token.oid}/presence/setUserPreferredPresence`, {
+        availability: availability,
+        activity: activity,
+        expirationDuration: duration,
+    }).then(() => {
+        console.info(`Your presence status has been set to ${availability} - ${activity}`);
+    }).catch(e => console.error(e));
+}
+
+export {}
